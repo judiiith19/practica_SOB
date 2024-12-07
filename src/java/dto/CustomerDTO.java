80d0cc829c048b48b5044f73852dd835404fec1f
Judith García <judith.garcias@estudiants.urv.cat>
2024-12-07 17:15:55 +0100

    Added CustomerDTO to dto

---
+++ src/java/dto/CustomerDTO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+import java.util.List;
+import model.entities.Link;
+
+/**
+ *
+ * @author JUDITH
+ */
+public class CustomerDTO {
+    private String username;
+    private Boolean isAuthor;
+    private String lastArticleLink; // Enlace al último artículo publicado.
+    
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    
+    public Boolean getIsAuthor() { return isAuthor; }
+    public void setIsAuthor(Boolean isAuthor) { this.isAuthor = isAuthor; }
+    
+    public String getLastArticleLink() { return lastArticleLink; }
+    public void setLastArticleLink(String link) { this.lastArticleLink = link; }
+}
